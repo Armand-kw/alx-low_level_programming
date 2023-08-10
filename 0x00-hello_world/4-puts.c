f6d9a9de411d9559ff0a4349ec30d73987d8357d
Armand <kwizeraarmand202@gmail.com>
2023-08-10 10:42:51 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#inlcude <stdio.h>
+/**
+ * Entry point - program that prints line with puts funcction
+ * Return: 0 
+ */
+int main (void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
